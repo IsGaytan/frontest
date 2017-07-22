ef25fde60467b0440f42359a481d6259c582900d
Is Gaytán SupPC <israelgaytan@supramax.com.mx>
2017-07-22 12:55:52 -0500

    First Commit.

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Title PW</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="Assets/css/styles_pw.css">
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+            <div class="col-lg-8 col-md-8 col-xs-12">
+                <div class="col-lg-12"></div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+            <div class="col-lg-8 col-md-8 col-xs-12">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">BUILD</a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active"><a href="#">PROYECTOS</a></li>
+                                <li class=""><a href="#">NOSOTROS</a></li>
+                                <li class=""><a href="#">SERVICIOS</a></li>
+                                <li class=""><a href="#">CONTÁCTANOS</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+        </div>
+        <div class="row" id="proyectos">
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+            <div class="col-lg-8 col-md-8 col-xs-12">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                        <li data-target="#myCarousel" data-slide-to="4"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="Assets/Hero/hero01.jpg" alt="hero01">
+                            <div class="carousel-caption">
+                                <p><b>SOLUCIONES CONSTRUCTIVAS</b></p>
+                                <p>SUSTENTABLES</p>
+                            </div>
+                            <div class="carousel-content">
+                                <p>Nuestro objetivo es la calidad de vida de la gente a través de construcciones sustentablesm calidad y seguridad Lider en Proyectos LEED</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="Assets/Hero/hero02.jpg" alt="hero02">
+                            <div class="carousel-caption">
+                                <p><b>SOLUCIONES CONSTRUCTIVAS</b></p>
+                                <p>SUSTENTABLES</p>
+                            </div>
+                            <div class="carousel-content">
+                                <p>Nuestro objetivo es la calidad de vida de la gente a través de construcciones sustentablesm calidad y seguridad Lider en Proyectos LEED</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="Assets/Hero/hero03.jpg" alt="hero03">
+                            <div class="carousel-caption">
+                                <p><b>SOLUCIONES CONSTRUCTIVAS</b></p>
+                                <p>SUSTENTABLES</p>
+                            </div>
+                            <div class="carousel-content">
+                                <p>Nuestro objetivo es la calidad de vida de la gente a través de construcciones sustentablesm calidad y seguridad Lider en Proyectos LEED</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="Assets/Hero/hero04.jpg" alt="hero04">
+                            <div class="carousel-caption">
+                                <p><b>SOLUCIONES CONSTRUCTIVAS</b></p>
+                                <p>SUSTENTABLES</p>
+                            </div>
+                            <div class="carousel-content">
+                                <p>Nuestro objetivo es la calidad de vida de la gente a través de construcciones sustentablesm calidad y seguridad Lider en Proyectos LEED</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="Assets/Hero/hero05.jpg" alt="hero05">
+                            <div class="carousel-caption">
+                                <p><b>SOLUCIONES CONSTRUCTIVAS</b></p>
+                                <p>SUSTENTABLES</p>
+                            </div>
+                            <div class="carousel-content">
+                                <p>Nuestro objetivo es la calidad de vida de la gente a través de construcciones sustentablesm calidad y seguridad Lider en Proyectos LEED</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+        </div>
+        <div id="nosotros" class="row">
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+            <div class="col-lg-8 col-md-8 col-xs-12">
+                <section>
+                    <h2>BUILD Building Systems</h2>
+                    <p>Construimos más de 700,000 m2 por año en edificios verticales.</p>
+                </section>
+                    
+                <ul class="nav nav-tabs">
+                    <li class="active">
+                        <a id="tab_ind" data-toggle="tab" href="#ind">
+                            <span class="icon"><i class="glyphicon glyphicon-oil"></i></span>
+                            <span class="text">N. INDUSTRIALES</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a id="tab_edi" data-toggle="tab" href="#edi">
+                            <span class="icon"><i class="glyphicon glyphicon-tower"></i></span>
+                            <span class="text">EDIFICIOS</span>
+                        </a>
+                    <li>
+                        <a id="tab_com" data-toggle="tab" href="#com">
+                            <span class="icon"><i class="glyphicon glyphicon-shopping-cart"></i></span>
+                            <span class="text">P. COMERCIALES</span>
+                        </a>
+                    <li>
+                        <a id="tab_inf" data-toggle="tab" href="#inf">
+                            <span class="icon"><i class="glyphicon glyphicon-road"></i></span>
+                            <span class="text">INFRAESTRUCTURA</span>
+                        </a>
+                    <li>
+                        <a id="tab_ret" data-toggle="tab" href="#ret">
+                            <span class="icon"><i class="glyphicon glyphicon-briefcase"></i></span>
+                            <span class="text">RETAIL</span>
+                        </a>
+                </ul>
+                <div class="tab-content">
+                    <div id="ind" class="tab-pane fade in active">
+                        <h3>INDUSTRIAL</h3>
+                        <p>Some content.</p>
+                    </div>
+                    <div id="edi" class="tab-pane fade in active">
+                        <h3>INDUSTRIAL</h3>
+                        <p>Some content.</p>
+                    </div>
+                    <div id="com" class="tab-pane fade in active">
+                        <h3>INDUSTRIAL</h3>
+                        <p>Some content.</p>
+                    </div>
+                    <div id="inf" class="tab-pane fade in active">
+                        <h3>INDUSTRIAL</h3>
+                        <p>Some content.</p>
+                    </div>
+                    <div id="ret" class="tab-pane fade in active">
+                        <h3>INDUSTRIAL</h3>
+                        <p>Some content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-xs-12"></div>
+        </div>
+        <div id="servicios" class="row">
+            <div class="col-lg-6">DISEÑO & CONSTRUCCIÓN</div>
+            <div class="col-lg-6">ENTREGA A TIEMPO</div>
+            <div class="col-lg-6">SEGURIDAD</div>
+            <div class="col-lg-6">BIM</div>
+        </div>
+        <div id="contactanos"></div>
+        <script>
+            /*if($( window ).width()<=840){
+                $('.nav-tabs').children('li').children('#tab_ind').html("<i class='glyphicon glyphicon-oil'></i>");
+                $('.nav-tabs').children('li').children('#tab_edi').html("<i class='glyphicon glyphicon-tower'></i>");
+                $('.nav-tabs').children('li').children('#tab_com').html("<i class='glyphicon glyphicon-shopping-cart'></i>");
+                $('.nav-tabs').children('li').children('#tab_inf').html("<i class='glyphicon glyphicon-road'></i>");
+                $('.nav-tabs').children('li').children('#tab_ret').html("<i class='glyphicon glyphicon-briefcase'></i>");
+            }else{
+                $('.nav-tabs').children('li').children('#tab_ind').html("<i class='glyphicon glyphicon-oil'></i> N. INDUSTRIALES");
+                $('.nav-tabs').children('li').children('#tab_edi').html("<i class='glyphicon glyphicon-tower'></i> EDIFICIOS");
+                $('.nav-tabs').children('li').children('#tab_com').html("<i class='glyphicon glyphicon-shopping-cart'></i> P. COMERCIALES");
+                $('.nav-tabs').children('li').children('#tab_inf').html("<i class='glyphicon glyphicon-road'></i> INFRAESTRUCTURA");
+                $('.nav-tabs').children('li').children('#tab_ret').html("<i class='glyphicon glyphicon-briefcase'></i> RETAIL");
+            }*/
+        </script>
+    </body>
+</html>
